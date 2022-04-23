071128cbd8b9cd8a09a96ebbf217a96a72a71795
andrewua85 <andrewua85@gmail.com>
2022-04-23 11:57:36 +0300

    View class

---
+++ vendor/Core/View.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Core;
+
+class View
+{
+    const VIEWS_DIR = 'Views';
+    /**
+     * name of template file
+     * @var string
+     */
+    protected $template = 'default';
+
+    /**
+     * @param string|null $template
+     */
+    public function __construct(string $template = null)
+    {
+        if ($template !== null){
+            $this->template = $template;
+        }
+    }
+
+    public function render(string $pageTemplate, array $data = []){
+        extract($data);
+        include_once 'vendor'.DIRECTORY_SEPARATOR.self::VIEWS_DIR.DIRECTORY_SEPARATOR.$this->template.'.php';
+
+    }
+
+}

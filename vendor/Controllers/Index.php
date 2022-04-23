e2edff7ce5609705a30e3cc97406048c4f9c0028
andrewua85 <andrewua85@gmail.com>
2022-04-23 11:05:21 +0300

    controller index add create and store

--- vendor/Controllers/Index.php
+++ vendor/Controllers/Index.php
@@ -7,4 +7,12 @@ class Index
     public function index(){
         echo 'index controller index action';
     }
+
+    public function create(){
+        echo 'index controller create action';
+    }
+
+    public function store(){
+        echo 'index controller store action';
+    }
 }

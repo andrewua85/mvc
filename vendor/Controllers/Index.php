3423f9fe688b9a6900944abf2f7233f44b5154e4
andrewua85 <andrewua85@gmail.com>
2022-04-23 11:08:55 +0300

    controller index delete create and store

--- vendor/Controllers/Index.php
+++ vendor/Controllers/Index.php
@@ -8,11 +8,4 @@ class Index
         echo 'index controller index action';
     }
 
-    public function create(){
-        echo 'index controller create action';
-    }
-
-    public function store(){
-        echo 'index controller store action';
-    }
 }

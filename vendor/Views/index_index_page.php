02c199a00af8eb35209303374f84ae2815c6f816
andrewua85 <andrewua85@gmail.com>
2022-04-23 11:58:39 +0300

    views default template and index_index_page

---
+++ vendor/Views/index_index_page.php
@@ -0,0 +1,7 @@
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam aperiam molestias numquam perferendis tempora
+    totam voluptatibus! A, ad aliquam asperiores consectetur eius et eum fuga ipsum iste magnam maxime modi
+    necessitatibus, omnis quo rem saepe sunt tempora. A alias culpa dignissimos enim, fugit inventore magni neque nobis
+    quae quis, quo repellendus reprehenderit? Adipisci cumque doloribus, inventore minus quod saepe suscipit! Aut autem
+    blanditiis corporis deleniti, deserunt error ipsam iure omnis quaerat quasi tenetur vitae voluptatem? A dolor
+    tempora voluptate?</p>

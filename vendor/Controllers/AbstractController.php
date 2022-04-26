f3c6db518ab94e9f0abe2562c30cf830270ca324
andrewua85 <andrewua85@gmail.com>
2022-04-26 19:21:33 +0300

    AbstractController

---
+++ vendor/Controllers/AbstractController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+
+use Core\View;
+
+abstract class AbstractController
+{
+    /*
+     * @var View
+     */
+    protected $view;
+
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+    abstract public function index();
+}
